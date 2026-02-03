549084d26c6f7b65dd0ff56ba7eaa2ab6d5409f0
Simon Wörpel <simon.woerpel@pm.me>
2026-02-03 06:02:01 +0100

    🔖 Bump version: 0.0.2 → 0.0.3

--- flydelta/__init__.py
+++ flydelta/__init__.py
@@ -3,7 +3,7 @@
 from flydelta.client import Client
 from flydelta.server import SERVER_DEPS_AVAILABLE
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __all__ = ["Client", "__version__"]
 
 if SERVER_DEPS_AVAILABLE:
